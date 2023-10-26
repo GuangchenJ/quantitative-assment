b3af1696900e5a8c3d0f479734912c2ed167e628
GuangchenJ <guangchen98.jiang@gmail.com>
2023-10-27 01:51:35 +0800

    perf(evol): accelerate the code about get stationary distribution and correspond matrix
    Change the type from `np.float64` to float

--- src/evol_quant_levels_matrix/stationary_dist_and_matrix.py
+++ src/evol_quant_levels_matrix/stationary_dist_and_matrix.py
@@ -18,36 +18,36 @@ def payoff_leading8_vs_all(_s0_num: int,
                            _pop_size: int,
                            _b: float,
                            _c: float,
-                           _all_com_p: np.ndarray) -> list[np.float64, np.float64]:
+                           _all_com_p: np.ndarray) -> list[float, float]:
     """
     payoffs: leading eight norm vs ALLC
     """
-    leading8_payoff = (np.float64(_b) * (_all_com_p[_s0_num - 1][0] * np.float64(_s0_num - 1) +
-                                         _all_com_p[_s0_num - 1][2] * np.float64(_pop_size - _s0_num)
-                                         ) / np.float64(_pop_size - 1)
-                       - np.float64(_c) * (
-                               _all_com_p[_s0_num - 1][0] * np.float64(_s0_num - 1) +
-                               _all_com_p[_s0_num - 1][1] * np.float64(_pop_size - _s0_num)
-                       ) / np.float64(_pop_size - 1))
-    all_payoff = (np.float64(_b) * (_all_com_p[_s0_num - 1][3] * np.float64(_pop_size - _s0_num - 1) +
-                                    _all_com_p[_s0_num - 1][1] * np.float64(_s0_num)
-                                    ) / np.float64(_pop_size - 1)
-                  - np.float64(_c) * (
-                          _all_com_p[_s0_num - 1][3] * np.float64(_pop_size - _s0_num - 1) +
-                          _all_com_p[_s0_num - 1][2] * np.float64(_s0_num)
-                  ) / np.float64(_pop_size - 1))
-    return [leading8_payoff, all_payoff]
+    leading8_payoff = (float(_b) * (_all_com_p[_s0_num - 1][0] * (float(_s0_num) - 1.) +
+                                    _all_com_p[_s0_num - 1][2] * (float(_pop_size) - float(_s0_num))
+                                    ) / (float(_pop_size) - 1.)
+                       - float(_c) * (
+                               _all_com_p[_s0_num - 1][0] * (float(_s0_num) - 1.) +
+                               _all_com_p[_s0_num - 1][1] * (float(_pop_size) - float(_s0_num))
+                       ) / (float(_pop_size) - 1.))
+    allx_payoff = (float(_b) * (_all_com_p[_s0_num - 1][3] * (float(_pop_size) - float(_s0_num) - 1.) +
+                                _all_com_p[_s0_num - 1][1] * float(_s0_num)
+                                ) / (float(_pop_size) - 1.)
+                   - float(_c) * (
+                           _all_com_p[_s0_num - 1][3] * (float(_pop_size) - float(_s0_num) - 1.) +
+                           _all_com_p[_s0_num - 1][2] * float(_s0_num)
+                   ) / (float(_pop_size) - 1.))
+    return [leading8_payoff, allx_payoff]
 
 
 def payoff_allc_vs_alld(_s0_num: int,
                         _pop_size: int,
                         _b: float,
-                        _c: float) -> list[np.float64, np.float64]:
+                        _c: float) -> list[float, float]:
     """
     payoffs: ALLC vs ALLD
     """
-    allc_payoff = np.float64(_b) * np.float64(_s0_num - 1) / np.float64(_pop_size - 1) - np.float64(_c)
-    alld_payoff = np.float64(_b) * np.float64(_s0_num) / np.float64(_pop_size - 1)
+    allc_payoff = float(_b) * (float(_s0_num) - 1.) / (float(_pop_size) - 1.) - float(_c)
+    alld_payoff = float(_b) * float(_s0_num) / (float(_pop_size) - 1.)
     return [allc_payoff, alld_payoff]
 
 
@@ -56,7 +56,7 @@ def get_payoff(_idx: int,
                _pop_size: int,
                _b: float,
                _c: float,
-               _all_com_p: np.ndarray = None) -> list[np.float64, np.float64]:
+               _all_com_p: np.ndarray = None) -> list[float, float]:
     """
     calling correct payoff function
     """
@@ -71,15 +71,15 @@ def get_payoff(_idx: int,
 def get_fixed_prob(_idx: int,
                    _args: dict,
                    _sys_args: argparse.Namespace,
-                   _all_com_p: np.ndarray = None) -> tuple[np.float64, np.float64]:
+                   _all_com_p: np.ndarray = None) -> tuple[float, float]:
     """
     calculating fixation probability of strategy M into strategy R from payoffs
     Ref: Eqn 2. in paper
     """
-    sum_q = np.float64(0.)
-    sum_q_invade = np.float64(0.)
-    remain = np.float64(1.)
-    invade = np.float64(1.)
+    sum_q = 0.
+    sum_q_invade = 0.
+    remain = 1.
+    invade = 1.
     for _k in range(1, _args["population size"]):
         pi_m, pi_r = get_payoff(
             _idx, _k, _args["population size"],
@@ -87,7 +87,7 @@ def get_fixed_prob(_idx: int,
             _args["reward matrix"]["cost of cooperation"],
             _all_com_p
         )
-        remain *= np.exp(-np.float64(_args["selection strength"]) * (pi_m - pi_r))
+        remain *= np.exp(-float(_args["selection strength"]) * (pi_m - pi_r))
         sum_q += remain
         pi_m_invade, pi_r_invade = get_payoff(
             _idx, _args["population size"] - _k, _args["population size"],
@@ -95,10 +95,10 @@ def get_fixed_prob(_idx: int,
             _args["reward matrix"]["cost of cooperation"],
             _all_com_p
         )
-        invade *= np.exp(-np.float64(_args["selection strength"]) * (pi_r_invade - pi_m_invade))
+        invade *= np.exp(-float(_args["selection strength"]) * (pi_r_invade - pi_m_invade))
         sum_q_invade += invade
-    xf = np.float64(1.) / (np.float64(1.) + sum_q)
-    xf_invade = np.float64(1.) / (np.float64(1.) + sum_q_invade)
+    xf = 1. / (1. + sum_q)
+    xf_invade = 1. / (1. + sum_q_invade)
     return xf, xf_invade
 
 
@@ -110,7 +110,7 @@ def get_stationary_dist_and_matrix(_allc_com_p: np.ndarray,
     """
     calculating selection-mutation equilibrium
     """
-    fixed_prob_matrix = np.zeros((3, 3), dtype=np.float64)
+    fixed_prob_matrix = np.zeros((3, 3)).astype(float)
     lc = get_fixed_prob(1, _args, _sys_args, _allc_com_p)
     ld = get_fixed_prob(2, _args, _sys_args, _alld_com_p)
     cd = get_fixed_prob(3, _args, _sys_args)
@@ -126,12 +126,12 @@ def get_stationary_dist_and_matrix(_allc_com_p: np.ndarray,
     fixed_prob_matrix[1][0] = lc[0]
     fixed_prob_matrix[2][0] = ld[0]
     fixed_prob_matrix[2][1] = cd[0]
-    fixed_prob_matrix = fixed_prob_matrix / np.float64(2.)
+    fixed_prob_matrix /= 2.
     for _i in range(3):
-        row_sum = np.float64(0.)
+        row_sum = 0.
         for _j in range(3):
             if _i != _j:
                 row_sum += fixed_prob_matrix[_i][_j]
-        fixed_prob_matrix[_i][_i] = np.float64(1.) - row_sum
+        fixed_prob_matrix[_i][_i] = 1. - row_sum
     mu = stationary_distribution(fixed_prob_matrix)
-    return mu, fixed_prob_matrix * np.float64(2.)
+    return mu, fixed_prob_matrix * 2.
